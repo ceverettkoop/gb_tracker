a720eba6e1a7498040cf1a9a58a3cb04fcb11c60
ceverettkoop <79296565+ceverettkoop@users.noreply.github.com>
2023-09-09 15:33:10 -0700

    stored notes i think

--- src/sound.asm
+++ src/sound.asm
@@ -2,14 +2,15 @@ INCLUDE "defines.asm"
 
 SECTION "Audio", ROMX
 
-;loop middle c on channel 1
+Init_Notes:
+
+
+;loop note on stack on ch 1
 Tone:
     ;NR10 = sweep, we are ignoring
-
     ;NR11 = length timer and duty cycle
     ld a, %10000000 ;50% duty cycle and 0 length timer
     ld [rNR11], a
-    
     ;NR12 = vol and envelope
     ld a, %11110000 ;max volume no envelope
     ld [rNR12], a
@@ -22,7 +23,14 @@ Tone:
     ;NR14 high bits
     ld a, %10000111
     ld [rNR14], a
+    $740
 
 Done:
     jp Done
 
+;upper and lower bytes to describe notes from C1 to C5, in just intonation
+;highest 4 bits represent note A = 0, A# = 1, B = 2, C = 3, C# = 4 etc
+SECTION "Note Data", ROMX
+    dw $05AE, $15CC, $25F0, $360A, $4624, $5642, $6659, $7674, $8688, $96A0, $A6B1, $B6C2 
+    dw $06D7, $16E6, $26F8, $3705, $4712, $5721, $672C, $773A, $8744, $9750, $A758, $B761 
+    dw $076B, $1773, $277C, $3782, $4789, $5790, $6796, $779D, $87A2, $97A8, $A7AC, $B7B0
